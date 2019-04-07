490ca5c69f0769d0a76db8dcc886f81b0644b2c2
Nikolay Koldunov <koldunovn@gmail.com>
2019-04-07 23:48:51 +0200

    more tests

--- tests/test_pyfesom2.py
+++ tests/test_pyfesom2.py
@@ -5,10 +5,13 @@
 
 import pytest
 import os
+import numpy as np
 
 
 from pyfesom2 import pyfesom2
 from pyfesom2 import load_mesh
+from pyfesom2 import get_data
+from pyfesom2 import fesom2regular
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 my_data_folder = os.path.join(THIS_DIR, 'data')
@@ -19,9 +22,56 @@ def test_readmesh():
     mesh = load_mesh(mesh_path, usepickle = False, usejoblib = False)
     assert mesh.n2d == 3140
     assert mesh.e2d == 5839
-    
+    mesh = load_mesh(mesh_path, usepickle = True, usejoblib = False)
+    assert os.path.exists(os.path.join(my_data_folder, 'pi-grid', 'pickle_mesh_py3_fesom2'))
+    os.remove(os.path.join(my_data_folder, 'pi-grid', 'pickle_mesh_py3_fesom2'))
+    mesh = load_mesh(mesh_path, usepickle = False, usejoblib = True)
+    assert os.path.exists(os.path.join(my_data_folder, 'pi-grid', 'joblib_mesh_fesom2'))
+    os.remove(os.path.join(my_data_folder, 'pi-grid', 'joblib_mesh_fesom2'))
+    mesh = load_mesh(mesh_path)
+    assert os.path.exists(os.path.join(my_data_folder, 'pi-grid', 'pickle_mesh_py3_fesom2'))
+    os.remove(os.path.join(my_data_folder, 'pi-grid', 'pickle_mesh_py3_fesom2'))
     print(mesh)
+
+def test_get_data():
+    mesh_path = os.path.join(my_data_folder, 'pi-grid')
+    data_path = os.path.join(my_data_folder, 'pi-results')
+    mesh = load_mesh(mesh_path, usepickle = False, usejoblib = False)
+    # variable on vertices
+    temp = get_data(data_path, 'temp', 1948, mesh)
+    assert type(temp) == np.ndarray
+
+    mmin = temp.min()
+    assert mmin == pytest.approx(-1.8924446)
+
+    mmax = temp.max()
+    assert  mmax == pytest.approx(28.816469)
     
+    # variable on elements
+    u = get_data(data_path, 'u', 1948, mesh)
+    assert type(u) == np.ndarray
+
+    mmin = u.min()
+    assert mmin == pytest.approx(-0.51486444)
+
+    mmax = u.max()
+    assert  mmax == pytest.approx(0.27181712)
+
+    # 2d variable on vertices
+    ice = get_data(data_path, 'a_ice', 1948, mesh)
+    assert type(u) == np.ndarray
+
+    mmean = ice.mean()
+    assert mmean == pytest.approx(0.27451384)
+
+
+
+def test_regriding():
+    mesh_path = os.path.join(my_data_folder, 'pi-grid')
+    data_path = os.path.join(my_data_folder, 'pi-results')
+    mesh = load_mesh(mesh_path, usepickle = False, usejoblib = False)
+
+
 
 @pytest.fixture
 def response():
