cd0e1e42694a53522363bc5d4353a0d120664dec
Nikolay Koldunov <koldunovn@gmail.com>
2019-11-30 10:06:16 +0100

    add volmean_data and tests

--- pyfesom2/diagnostics.py
+++ pyfesom2/diagnostics.py
@@ -3,9 +3,11 @@
 # This file is part of pyfesom2
 # Original code by Nikolay Koldunov, Dmitry Sidorenko and Qiang Wang (2019)
 
+import os
 import numpy as np
 import xarray as xr
-import os
+from .load_mesh_data import ind_for_depth
+
 
 
 def add_timedim(data, date="1970-01-01"):
@@ -15,7 +17,7 @@ def add_timedim(data, date="1970-01-01"):
             raise ValueError(
                 "You trying to add time dimension to the DataArray that already have it. \
 The reason migh be that you trying to use 2d variable (e.g. `a_ice`) \
-in a function that accepts only 3d variables (e.g. `volume_hovm`)"
+in a function that accepts only 3d variables (e.g. `hovm_data`)"
             )
         timestamp = [np.array(np.datetime64(date, "ns"))]
         data = data.expand_dims({"time": timestamp}, axis=0)
@@ -176,22 +178,21 @@ def get_meshdiag(mesh, meshdiag=None, runid="fesom"):
     if meshdiag:
         if os.path.exists(meshdiag):
             diag = xr.open_dataset(meshdiag)
-            return diag
         else:
             raise Exception("File {} does not exist.".format(meshdiag))
     elif not meshdiag:
         path_to_meshdiag = os.path.join(mesh.path, "{}.mesh.diag.nc".format(runid))
         if os.path.exists(path_to_meshdiag):
             diag = xr.open_dataset(path_to_meshdiag)
-            return diag
         else:
             raise Exception(
                 "File {} does not exist. Please provide explicit path\
-                             to diag file (e.g. fesom.mesh.diag), or put it to the mesh directory.\
+                to diag file (e.g. fesom.mesh.diag), or put it to the mesh directory.\
                             ".format(
                     path_to_meshdiag
                 )
             )
+    return diag
 
 
 def hovm_data(data, mesh, meshdiag=None, runid="fesom"):
@@ -205,20 +206,26 @@ def hovm_data(data, mesh, meshdiag=None, runid="fesom"):
      data: xarray.DataArray, numpy.array
         Input data, that can be ether xarray data, or just numpy array,
         with values of 3d tracer variable (on nodes).
-        Input shouls be 3d (time, nodes, levels(nz1))
-        The 2D input in form of (nodes, levels(nz1)) is possible, but not recomended :)
+        Input should be 3d (time, nodes, levels(nz1))
+        The 2D input (to calculate value for only one time step)
+        in form of (nodes, levels(nz1)) is possible, but not recomended :)
     mesh: mesh object
         FESOM2 mesh object.
     meshdiag: str
         path to *mesh.diag.nc file, that is created during fesom cold start.
     runid: str
         name of the run. Usually just `fesom`.
+
+    Returns
+    -------
+    time series: xarray.DataArray
+        time series of area weighted vertical profiles of scalar values.
     """
 
     if len(data.shape) == 2:
         data = add_timedim(data)
 
-    diag = get_meshdiag(mesh, meshdiag)
+    diag = get_meshdiag(mesh, meshdiag, runid)
     nod_area = diag.rename_dims({"nl": "nz1", "nod_n": "nod2"}).nod_area
     if isinstance(data, xr.DataArray):
         hdg_total = (data * nod_area[:-1, :].T).sum(dim="nod2")
@@ -232,18 +239,35 @@ def hovm_data(data, mesh, meshdiag=None, runid="fesom"):
 
 
 def select_depths(uplow, mesh):
+    """ Select indexes of depths between upper and lower bound.
+
+    Parameters
+    ----------
+    uplow: list
+        if None, all depths will be selected
+        if e.g. [2000, 'depth'] all from model depth closest to 200 down to depth will be selected
+        if e.g. [0, 700] all between 0 and closest depth to 700 will be selected.
+        if e.g. [500, 500] only model level closest to 500 will be selected.
+    mesh: mesh object
+        FESOM2 mesh object.
+
+    Returns
+    -------
+    indexes: range
+        range of depth indexes.
+    """
     if not uplow:
         indexes = range(mesh.nlev - 1)
         return indexes
     elif uplow[1] == "bottom":
-        upper = pf.ind_for_depth(uplow[0], mesh)
+        upper = ind_for_depth(uplow[0], mesh)
         lower = mesh.nlev - 1
         print(f"Upper depth: {mesh.zlev[upper]}, Lower depth: bottom")
         indexes = range(upper, lower)
         return indexes
     else:
-        upper = pf.ind_for_depth(uplow[0], mesh)
-        lower = pf.ind_for_depth(uplow[1], mesh)
+        upper = ind_for_depth(uplow[0], mesh)
+        lower = ind_for_depth(uplow[1], mesh)
         if lower >= mesh.nlev - 2:
             lower = mesh.nlev - 2
         print(f"Upper depth: {mesh.zlev[upper]}, Lower depth: {mesh.zlev[lower]}")
@@ -251,21 +275,50 @@ def select_depths(uplow, mesh):
         return indexes
 
 
-def volume_int(data, mesh, meshdiag=None, runid="fesom", uplow=None):
-    if len(data.shape) == 2:
-        data = pf.add_timedim(data)
+def volmean_data(data, mesh, uplow=None, meshdiag=None, runid="fesom", ):
+    """Calculate volume weighted mean over the range of depths.
 
-    diag = pf.get_meshdiag(mesh, meshdiag)
+    Parameters
+     ----------
+     data: xarray.DataArray, numpy.array
+        Input data, that can be ether xarray data, or just numpy array,
+        with values of 3d tracer variable (on nodes).
+        Input should be 3d (time, nodes, levels(nz1))
+        The 2D input (to calculate value for only one time step)
+        in form of (nodes, levels(nz1)) is possible, but not recomended :)
+    mesh: mesh object
+        FESOM2 mesh object.
+    uplow: list
+        if None, all depths will be selected
+        if e.g. [2000, 'depth'] all from model depth closest to 200 down to depth will be selected
+        if e.g. [0, 700] all between 0 and closest depth to 700 will be selected.
+        if e.g. [500, 500] only model level closest to 500 will be selected.
+    meshdiag: str
+        path to *mesh.diag.nc file, that is created during fesom cold start.
+    runid: str
+        name of the run. Usually just `fesom`.
+
+    Returns
+    -------
+    time series: xarray.DataArray
+        time series (or one point) of volume weighted scalar.
+    """
+    if len(data.shape) == 2:
+        data = add_timedim(data)
+
+    diag = get_meshdiag(mesh, meshdiag, runid)
     nod_area = diag.rename_dims({"nl": "nz1", "nod_n": "nod2"}).nod_area
+    delta_z=np.abs(np.diff(mesh.zlev))
 
     indexes = select_depths(uplow, mesh)
 
-    totalT = 0.0
-    totalV = 0.0
+    total_t = 0.0
+    total_v = 0.0
+    # we calculate layer by layer
     for i in indexes:
         aux = (data[:, :, i] * nod_area[i, :].data).sum(axis=1)
-        totalT = totalT + aux * dz[i]
-        totalV = totalV + nod_area[i, :].data.sum() * dz[i]
+        total_t = total_t + aux * delta_z[i]
+        total_v = total_v + nod_area[i, :].data.sum() * delta_z[i]
 
-    return totalT / totalV
+    return total_t / total_v
 
