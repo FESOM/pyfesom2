0cf1f6fe8cc6395dde7dccf552b07afa0c17f70e
Nikolay Koldunov <koldunovn@gmail.com>
2020-01-29 22:54:24 +0100

    bring autodoc back

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,9 @@ import pyfesom2
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [ 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+
+autodoc_mock_imports = ['mpl_toolkits', 'Basemap', 'cartopy']
 
 #autodoc_mock_imports = ['matplotlib', 'mpl_toolkits', 'Basemap', 'cartopy']
 
@@ -86,8 +88,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
