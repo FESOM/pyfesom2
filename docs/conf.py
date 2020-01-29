e32b0ff3359fe40d242df38c6a6c15096da13b02
Nikolay Koldunov <koldunovn@gmail.com>
2020-01-29 20:58:02 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ import pyfesom2
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [ 'sphinx.ext.viewcode']
 
-autodoc_mock_imports = ['matplotlib', 'mpl_toolkits', 'Basemap', 'cartopy']
+#autodoc_mock_imports = ['matplotlib', 'mpl_toolkits', 'Basemap', 'cartopy']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
