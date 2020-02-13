a50a5dbe65843510cc423c1326400a7e51f99c62
alireza mahdavi <alireza.mahdavi@awi.de>
2020-02-13 13:29:37 +0100

    pep8

--- pyfesom2/fesom2GeoFormat.py
+++ pyfesom2/fesom2GeoFormat.py
@@ -1,15 +1,9 @@
 from pyfesom2 import fesom2regular
-from osgeo import osr,gdal
-import numpy as np 
+from osgeo import osr, gdal
+import numpy as np
 
 
-
-
-
-
-
-
-def fesom2GeoFormat(data,mesh,outName,radius_of_influence=100000,methode='nn', driver='Gtiff'):
+def fesom2GeoFormat(data, mesh, outName, radius_of_influence=100000, methode='nn', driver='Gtiff'):
     """ Returns Fesom as Gdal Geotiff or Gdal NetCDF .
 
     Parameters:
@@ -23,28 +17,28 @@ def fesom2GeoFormat(data,mesh,outName,radius_of_influence=100000,methode='nn', d
         Gdal Datasets:Gtiff or NetCDF.   
 
     """
-    lonreg,latreg,rasterOrigin = grid(mesh)
-    data2= dataShape(data)
+    lonreg, latreg, rasterOrigin = grid(mesh)
+    data2 = dataShape(data)
     pixelWidth = 1
     pixelHeight = -1
     outputName = outName
-    array =  fesom2regular(
-            data2,
-            mesh,
-            lonreg,
-            latreg,
-            distances_path=None,
-            inds_path=None,
-            qhull_path=None,
-            how=methode,
-            k=5,
-            radius_of_influence=radius_of_influence,
-            n_jobs=2,
-            dumpfile=True,
-            basepath=None,
-            )
-    
-    reversed_arr = array[::-1] # reverse array so the tif looks like the array
+    array = fesom2regular(
+        data2,
+        mesh,
+        lonreg,
+        latreg,
+        distances_path=None,
+        inds_path=None,
+        qhull_path=None,
+        how=methode,
+        k=5,
+        radius_of_influence=radius_of_influence,
+        n_jobs=2,
+        dumpfile=True,
+        basepath=None,
+    )
+
+    reversed_arr = array[::-1]  # reverse array so the tif looks like the array
 
     array2raster(
         outputName,
@@ -52,33 +46,31 @@ def fesom2GeoFormat(data,mesh,outName,radius_of_influence=100000,methode='nn', d
         pixelWidth,
         pixelHeight,
         reversed_arr,
-        driver) #convert array to raster
-
-
-
-
+        driver)  # convert array to raster
 
 
 def dataShape(data):
-    if (len(data.shape)>1):
-        data=data.flatten()
+    if (len(data.shape) > 1):
+        data = data.flatten()
     else:
-        data=data
-    
+        data = data
+
     return data
 
+
 def grid(mesh):
-    xmin,ymin,xmax,ymax = [mesh.x2.min(),mesh.y2.min(),mesh.x2.max(),mesh.y2.max()]
-    res=[int(round(abs(xmin)+abs(xmax))),int(round(abs(ymin)+abs(ymax)))]
+    xmin, ymin, xmax, ymax = [
+        mesh.x2.min(), mesh.y2.min(), mesh.x2.max(), mesh.y2.max()]
+    res = [int(round(abs(xmin)+abs(xmax))), int(round(abs(ymin)+abs(ymax)))]
     lonNumber, latNumber = res
-    lonreg = np.linspace(xmin,xmax, lonNumber)
-    latreg = np.linspace(ymin,ymax, latNumber)
-    lonreg,latreg = np.meshgrid(lonreg,latreg)
-    rasterOrigin = (xmin,ymax)
-    return lonreg,latreg,rasterOrigin
+    lonreg = np.linspace(xmin, xmax, lonNumber)
+    latreg = np.linspace(ymin, ymax, latNumber)
+    lonreg, latreg = np.meshgrid(lonreg, latreg)
+    rasterOrigin = (xmin, ymax)
+    return lonreg, latreg, rasterOrigin
 
 
-def array2raster(outputName,rasterOrigin,pixelWidth,pixelHeight,array,driver='NetCDF'):
+def array2raster(outputName, rasterOrigin, pixelWidth, pixelHeight, array, driver='NetCDF'):
 
     cols = array.shape[1]
     rows = array.shape[0]
@@ -88,7 +80,8 @@ def array2raster(outputName,rasterOrigin,pixelWidth,pixelHeight,array,driver='Ne
     driver = gdal.GetDriverByName(driver)
     outRaster = driver.Create(outputName, cols, rows, 1, gdal.GDT_Byte)
 #   outRaster.SetGeoTransform(geotransform)
-    outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
+    outRaster.SetGeoTransform(
+        (originX, pixelWidth, 0, originY, 0, pixelHeight))
     outband = outRaster.GetRasterBand(1)
     outband.WriteArray(array)
     outRasterSRS = osr.SpatialReference()
