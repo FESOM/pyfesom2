9a4d807597b29de730329bb6b473f88b12f1df88
suvarchal <suvarchal.kumar@gmail.com>
2020-12-08 11:38:26 +0100

    optimize imports and mild refactoring

--- tests/test_accessor.py
+++ tests/test_accessor.py
@@ -1,11 +1,12 @@
-from pyfesom2.datasets import LCORE, A01
-from pyfesom2.accessor import FESOMDataArray
 import pytest
-from shapely.geometry import box
 import xarray as xr
+from shapely.geometry import box
 
-#TODO: skipping A01 because chunksize is too small, leading to slow sel
-@pytest.fixture(scope='module', params=[LCORE])  #, A01])
+from pyfesom2.datasets import LCORE
+
+
+# TODO: skipping A01 because chunksize is too small, leading to slow sel
+@pytest.fixture(scope='module', params=[LCORE])  # , A01])
 def dataset(request):
     da = request.param.load()
     yield da
@@ -17,19 +18,19 @@ def test_dataarray_accessor(dataset):
     assert hasattr(dataset[vars[0]], "pyfesom2"), "dataarray doesn't have accessor pyfesom2"
     # check if region selection works
     # bounding box selection using list
-    bbox = [0.,60., 60., 90.]
+    bbox = [0., 60., 60., 90.]
     box_sel = dataset[vars[0]].pyfesom2.select_region(region=bbox)
     assert isinstance(box_sel, xr.DataArray), "Region selection using bbox list failed"
     # TODO: check if bounda match
     # Polygon selection
-    bbox = box(*bbox) # Polygon
+    bbox = box(*bbox)  # Polygon
     box_sel = dataset[vars[0]].pyfesom2.select_region(region=bbox)
     assert isinstance(box_sel, xr.DataArray), "Region selection using Polygon failed"
     # Point selection
-    point_sel = dataset[vars[0]].pyfesom2.select_point(lon=0,lat=0)
+    point_sel = dataset[vars[0]].pyfesom2.select_point(lon=0, lat=0)
     assert isinstance(point_sel, xr.DataArray)
 
-    point_sel = dataset[vars[0]].pyfesom2.select_point(lon=0,lat=0, tolerance=10)
+    point_sel = dataset[vars[0]].pyfesom2.select_point(lon=0, lat=0, tolerance=10)
     assert isinstance(point_sel, xr.DataArray)
 
     with pytest.raises(NotImplementedError):
@@ -43,6 +44,5 @@ def test_dataarray_accessor(dataset):
     assert isinstance(sel_arr, xr.DataArray)
 
     if "time" in dataset.dims:
-        mintime,maxtime = dataset.time.min(), dataset.time.max()
+        mintime, maxtime = dataset.time.min(), dataset.time.max()
         sel_arr = dataset[vars[0]].pyfesom2.sel(time=mintime, region=bbox)
-
