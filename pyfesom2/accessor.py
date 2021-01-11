2cf8f6aa25b4e536fd48f31e6be1b9a056d96090
suvarchal <suvarchal.kumar@gmail.com>
2021-01-11 05:23:37 +0100

    add accessor design thoughts and issues in module doc

--- pyfesom2/accessor.py
+++ pyfesom2/accessor.py
@@ -1,5 +1,55 @@
-## support sel on remote
-## and opened dataset
+"""
+Pyfesom2 Xarray Accessors
+
+This module provides additional functionality to FESOM2 xarray dataset. This additional functionality adds
+selection, plotting, regridding for unstructured FESOM2 grid. These features are implemted using neat xarray accessor
+protocol.
+
+Examples:
+    from pyfesom2 import open_dataset
+    fesom_xr_dataset = open_dataset(data_path, mesh_path, ...)
+    fesom_xr_dataset.pyfesom2.sel(...)
+    fesom_xr_dataset.pyfesom2.plot(...)
+
+Design Considerations:
+Goal is to provide commonly used xarray methods (like sel, plot... ) that are currently un-supported for unstructured
+FESOM model data. We would like to have these methods available on xarray dataset level and dataarray (data variable).
+
+Implementing this would mean that we need to register a "pyfesom2" dataset and a dataarray accessor
+(http://xarray.pydata.org/en/stable/api.html#advanced-api). Accessor on dataset is for convinince to do operations like
+selection all variables and this is also efficient (more on that below), accessor on dataarray is most instutuve and
+most apt for plotting: for example dataset.variable.pyfesom2.plot(..).
+
+Having both dataset and datarray working on same kind of data has issues:
+ 1.) It creates a not-so-desirable user interface for certain operations, for instance, to select a region on dataset and
+plot a variable: dataset.pyfesom2.sel(region.).variable.pyfesom2.plot(...).
+ 2.) Most user intuitive interface to plotting is via: ...variable.plot(...). For variables on this unstructured grid,
+ generating spatial plots would need node connectivity information without which land and oceans cannot be shown
+accurately and this additional information (like vertices(3, nelems)) cannot be part of coordinates of dataarray of
+variable. Xarray's dataarray object can only hold  coordinates for dims of variable(while a dataset can hold any arbitary
+ coordinate information ). This would user interface to spatial plotting of variable should get mesh information
+ externally, either as argument in plot method or path to mesh in dataarray's attribute. That is inefficient among
+ other UI issues.
+ 3.) Selection on this unstructured grid is implemented using a tree, building a tree can be expensive depending on
+ grid size. When using selection on a variable algorithm would build a tree and we would like to utilize that tree in
+ selections on other variables in future. And to retain mesh info after a selection (see 2.) we should return a dataset,
+ this is in contrast to xarray's default selection on a variable returning a dataarray.
+
+Following implementation takes these and user interface into consideration:
+pyfesom2 is registered as xarray dataset accessor only, access to data variables from this accessor are controlled
+through a wrapping class to data variable (instead of dataarray accessor) that contains context of
+dataset (thereby node info and able to share sel tree). Logic of wrapping data variable in a class is what accessors
+are behind the scenes in xarrray, except in this case we pass dataset context to the constructor.
+
+example access patterns would be:
+dataset.pyfesom2.sel(...) , dataset.pyfesom2.variable.sel(...)
+dataset.pyfesom2.variable.triplot()
+
+One other way to think for future would be to subclass xarray's Dataset and DataArray against their recommendation
+but might be worth it in this case. It might need painful re-implement/tweak xarray's open_dataset kind of methods.
+Another option could be to have a datastore for fesom data (not well-thought.
+"""
+
 from collections.abc import Sequence  # python>3.3?
 from typing import Union, Sequence as SequenceType
 
@@ -13,7 +63,6 @@ from shapely.geometry import MultiPoint, MultiPolygon, Polygon, box
 # New Types
 BoundingBox = SequenceType[float]
 MultiRegion = Union[SequenceType[Polygon], MultiPolygon]
-
 
 @xr.register_dataarray_accessor("pyfesom2")
 class FESOMDataArray(object):
