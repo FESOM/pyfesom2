4d1e8490025e26b118f0b24aebb281c90a919439
suvarchal <suvarchal.kumar@gmail.com>
2021-02-04 07:53:57 +0100

    update to selection, trimesh plot, cleanup

--- pyfesom2/accessor.py
+++ pyfesom2/accessor.py
@@ -55,6 +55,7 @@ TODO: possibly add projection as a setting flag.
       set plot type interactive or not as setting.
 """
 
+import functools
 from collections.abc import Sequence  # python>3.3?
 from typing import Union, Sequence as SequenceType, Tuple
 
@@ -63,7 +64,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
 from matplotlib.tri import Triangulation
-from shapely.geometry import MultiPoint, MultiPolygon, Polygon, box
+from shapely.geometry import MultiPolygon, Polygon, asMultiPoint
 
 # New Types
 BoundingBox = SequenceType[float]
@@ -110,52 +111,75 @@ def select_points(xrobj: Union[xr.Dataset, xr.DataArray],
     return xrobj.isel(nod2=ind)
 
 
-def select_region(xrobj: Union[xr.Dataset, xr.DataArray],
-                  region: Union[BoundingBox, Polygon]) -> Union[xr.Dataset, xr.DataArray]:
-    """
-    Selects data for a region. region can be specified as a Bounding Box [West, Soutn, North, East ] or
-    as shapely polygons or for multi region selection: a list of Polygons or MultiPolygon.
+class SimpleMesh:
+    """Wrapper that fakes pyfesom's mesh for purposes of this module"""
 
-    This uses xarray's standard methods on stacked data
-    TODO: handle multi polygons as list or Multipolygon
-    Parameters
-    ----------
-    region
+    def __init__(self, lon, lat, faces):
+        self.x2 = lon
+        self.y2 = lat
+        self.elem = faces
 
-    Can be bounding box or shapely polygon
 
-    Returns
-    -------
-    xr.DataArray
-    """
-    # import pandas as pd
+def select_bbox(ds: xr.Dataset, bbox: BoundingBox) -> xr.Dataset:
+    """bbox as xmin,xmax, ymin, ymax"""
+    from .ut import cut_region
+    data = ds.drop('faces')
+    mesh = SimpleMesh(ds.lon, ds.lat, ds.faces)
+    cut_faces = cut_region(mesh, bbox)
+    uniq, inv_index = np.unique(cut_faces.faces.values.ravel(), return_inverse=True)
+    new_faces = inv_index.reshape(cut_faces.shape)
+    ret = data.isel(nod2=uniq)
+    ret = ret.assign_coords({'faces': (('nelem', 'three'), new_faces)})
+    return ret
+
+
+def select_region(xrobj: xr.Dataset,
+                  region: Union[BoundingBox, Polygon]) -> xr.Dataset:
+    from shapely.geometry import box, Polygon
     if isinstance(region, Sequence) and len(region) == 4:
         sel_polygon = box(*region)
     elif isinstance(region, Polygon):
         sel_polygon = region
     else:
         raise NotImplementedError(f'Supplied region data {region} is not yet supported')
-    data = xrobj
-    # if not isinstance(xrobj.indexes['nod2'], pd.MultiIndex):
-    if 'nod2' not in data.indexes:  # cheaper way then importing pandas
-        data = xrobj.set_index(nod2=['lon', 'lat'])
+    left, down, right, top = sel_polygon.bounds
+    sel = select_bbox(xrobj, [left, right, down, top])
+    if sel_polygon == sel_polygon.envelope:
+        return sel
 
-    mesh_as_mp = MultiPoint(data.nod2.values.T)  # should we save it for subsequent selections?, profile this.
-    # self._mesh_as_mp = asMultiPoint(self.stacked_data.nod2.values.T) # faster but net is slower why?, profile this
+    as_mp = asMultiPoint(np.vstack((sel.lon, sel.lat)).T)  # asMultiPoint saves memory according to docs
 
-    # selection by indexes # in efficient because of list comprehension although isel is efficient.
-    # inds = [i for i, pt in enumerate(mesh_as_mp) if pt.within(sel_polygon)]
-    # #inds = list(set(inds)) # there are a few grids with dulplicates
-    # return data.isel(nod2=inds)
+    vals = np.asarray(as_mp.intersection(sel_polygon))
 
-    sel_indexer = np.asarray(mesh_as_mp.intersection(sel_polygon))
-    sel_indexer = list((lo, la) for lo, la in sel_indexer)
-    # sel_indexer = list(set((lo, la) for lo, la in sel_indexer))  # removing duplicates
+    in_lon, in_lat = vals.T
+    sel_inds = (
+        np.isin(sel.lon.values, in_lon, assume_unique=True) & np.isin(sel.lat.values, in_lat, assume_unique=True))
 
-    if len(sel_indexer) == 0:
-        raise Exception('No region found')
+    mesh = SimpleMesh(sel.lon, sel.lat, sel.faces)
+    nod2_inds = np.nonzero(sel_inds)  # nonzero works only for bool arrays to get inds
+    # indices of nod2 that correspond to polygon
+    new_elem = mesh.elem[np.mean(np.isin(mesh.elem, nod2_inds), axis=1) == 1]
+    uniq, inv_index = np.unique(new_elem.values.ravel(), return_inverse=True)
+    new_faces = inv_index.reshape(new_elem.shape)
+    ret = sel.isel(nod2=uniq)
+    ret = ret.assign_coords({'faces': (('nelem', 'three'), new_faces)})
+    return ret
 
-    return data.sel(nod2=sel_indexer)
+
+def sel_nn_points(ds, lon, lat):
+    lon, lat = np.asarray(lon), np.asarray(lat)
+    bounds = (lon.min(), lon.max(), lat.min(), lat.max())
+    sel = select_bbox(ds, bounds)
+    tris = Triangulation(sel.lon, sel.lat, triangles=sel.faces)
+    tf = tris.get_trifinder()
+    inds = tf(lon, lat)
+    new_faces = sel.faces[inds[inds > -1]]
+    inds, inv_index = np.unique(new_faces.values.ravel(), return_inverse=True)
+    new_faces = inv_index.reshape(new_faces.shape)
+    ret = sel.isel(nod2=inds)
+    ret = ret.drop('faces')
+    ret = ret.assign_coords({'faces': (('nelem', 'three'), new_faces)})
+    return ret
 
 
 def select(xrobj: Union[xr.Dataset, xr.DataArray], method='nearest',
@@ -170,7 +194,6 @@ def select(xrobj: Union[xr.Dataset, xr.DataArray], method='nearest',
     region
     path
     indexers
-
     Returns
     -------
 
@@ -180,17 +203,23 @@ def select(xrobj: Union[xr.Dataset, xr.DataArray], method='nearest',
     lat_indexer = True if lat is not None else False
     lon_indexer = True if lon is not None else False
 
-    if not method == "nearest":
-        raise NotImplementedError("Only nearest method is currently supported")
     if (lat_indexer or lon_indexer) and (region is not None or path is not None):
         # TODO: do this combinations better, doesn't check if path and region are both given
         raise Exception("Onlu one option: lat, lon as indexer or path or region is supported")
 
     if lat_indexer or lon_indexer:
         if lat_indexer and lon_indexer:
-            ret_arr = select_points(xrobj, lon, lat, method=method, tolerance=tolerance)
+            if method == 'nearest':
+                if tolerance:
+                    raise NotImplementedError('tolerance is not supported for method: nearest, try method:'
+                                              'projected_nearest')
+                ret_arr = sel_nn_points(xrobj, lon, lat)
+            elif method == 'projected_nearest':
+                ret_arr = select_points(xrobj, lon, lat, method=method, tolerance=tolerance)
+            else:
+                raise NotImplementedError("Only methods 'nearest, projected_nearest' are currently supported")
         else:
-            raise NotImplementedError("Both lat, lon are needed as indexer, else use path or region")
+            raise NotImplementedError("Both lat, lon are needed as indexers, else use path or region")
     elif region is not None:
         ret_arr = select_region(xrobj, region)
     elif path is not None:
@@ -211,15 +240,10 @@ class FESOMDataset:
         self._tree = None
         for datavar in xr_obj.data_vars:
             # setattr(self, str(datavar), xr_obj[datavar])
-            setattr(self, str(datavar), FESOMDataArray2(xr_obj[datavar], xr_obj))
+            setattr(self, str(datavar), FESOMDataArray(xr_obj[datavar], xr_obj))
 
     def select(self, method='nearest', tolerance=None, region=None, path=None, **indexers):
-        self._xrobj[
-            'nod2ind'] = ('nod2',np.array(self._xrobj.nod2.values))  # add nod2 values so that after selection
-        # indicies in faces still point to right nodes
-        self._xrobj[
-            'nod2ind2'] = ('nod2',np.asarray(self._xrobj.nod2.values))  # add nod2 values so that after selection
-        sel_obj = select(self._xrobj, method='nearest', tolerance=None, region=None, path=None, **indexers)
+        sel_obj = select(self._xrobj, method='nearest', tolerance=tolerance, region=region, path=path, **indexers)
         return sel_obj
 
     def regrid(self):
@@ -255,8 +279,18 @@ class FESOMDataset:
     def _repr_html_(self):
         return self._xrobj._repr_html_()
 
+def trimesh_plot(darr,tris, time, nz1):
+    import geoviews as gv
+    tris = np.asarray(tris)
+    data = darr.sel(time=time,nz1=nz1)
+    var_name = data.name
+    var_da =  gv.Dataset((darr.lon,darr.lat,data),
+                         kdims=['lon','lat'], vdims=[var_name])
+    return gv.TriMesh((tris, var_da))
 
-class FESOMDataArray2:
+class FESOMDataArray:
+    """ A Awapper around Dataarray"""
+
     def __init__(self, xr_dataarray: xr.DataArray, context_dataset=None):
         self._xrobj = xrobj = xr_dataarray
         self._context_dataset = context = context_dataset
@@ -299,6 +333,13 @@ class FESOMDataArray2:
 
         ax = kwargs.pop('ax', plt.axes(projection=projection))
 
+        if 'extents' in kwargs:
+            ax.set_extent(kwargs.pop('extents'), crs=ccrs.PlateCarree())
+
+        if 'extents' in kwargs:
+            ax.set_extent(kwargs.pop('extents'), crs=ccrs.PlateCarree())
+            ax.set_global()
+
         minv, maxv = data.min().values, data.max().values
 
         # pl=ax.tricontourf(tri, data, levels=np.linspace(minv, maxv,100))
@@ -333,6 +374,9 @@ class FESOMDataArray2:
 
         ax = kwargs.pop('ax', plt.axes(projection=projection))
 
+        if 'extents' in kwargs:
+            ax.set_extent(kwargs.pop('extents'), crs=ccrs.PlateCarree())
+
         minv, maxv = data.min().values, data.max().values
 
         # pl=ax.tricontourf(tri, data, levels=np.linspace(minv, maxv,100))
@@ -352,11 +396,14 @@ class FESOMDataArray2:
         projection, tri = self._triangulate(data, projection)
 
         ax = kwargs.pop('ax', plt.axes(projection=projection))
+
+        if 'extents' in kwargs:
+            ax.set_extent(kwargs.pop('extents'), crs=ccrs.PlateCarree())
+
         return ax.triplot(tri, *args, **kwargs)
 
     def trimesh(self, levels=None, cmap='RdBu', colorbar=True, height=350, width=600,
                 colorbar_position="bottom", projection=None, tools=['hover'], **hv_kwopts):
-
         try:
             import geoviews as gv
             import holoviews as hv
@@ -365,186 +412,39 @@ class FESOMDataArray2:
         except ImportError as ex:
             raise ImportError('Using trimesh needs geoviews[holoviews, bokeh] and datashader') from ex
 
-        data = self._xrobj
-        var_name = data.name
-        projection, tri = self._triangulate(data, ccrs.PlateCarree())  # not necessary
-        tris = gv.Dataset(tri.triangles, kdims=['v0', 'v1', 'v2'])
-        verts = gv.Dataset((tri.x, tri.y, data), kdims=['lon', 'lat'], vdims=[var_name])
-        plot = gv.TriMesh((tris, verts))
+        trimesh_fn = functools.partial(trimesh_plot, darr=self._xrobj, tris=self._context_dataset.faces)
+        var_name = self._xrobj.name
+        hvd = hv.Dataset(self._xrobj.drop_vars(['lon','lat']))
+        dmap = hv.DynamicMap(trimesh_fn,
+                             kdims=hvd.kdims).redim.values(nz1=self._xrobj.nz1.values,
+                                                           time=self._xrobj.time.values)
+        dmap = dmap.redim.default(nz1=self._xrobj.nz1.values[0])
+        dmap = dmap.redim.unit(nz1='m')
+        dmap = dmap.redim.label(nz1='level')
 
         plot_opts = {**hv_kwopts}
         if projection:
             plot_opts.update({'projection': projection})
-
+        else:
+            plot_opts.update({'projection': ccrs.PlateCarree()})
         if levels:
             if isinstance(levels, int):
                 plot_opts.update({'color_levels': levels})
             elif isinstance(levels, Sequence):
                 if len(levels) > 2:
                     plot_opts.update({'color_levels': len(levels)})
-                    plot = plot.redim.range(var_name=(min(levels), max(levels)))
+                    dmap = dmap.redim.range(var_name=(min(levels), max(levels)))
                 elif len(levels) == 2:
-                    plot = plot.redim.range(var_name=levels)
+                    dmap = dmap.redim.range(var_name=levels)
             else:
                 raise Exception('Invalid levels, should be a tuple with limits or a sequence of values')
-
-        return rasterize(plot).opts(tools=tools, width=width,
+        else:
+            dmap = dmap.opts(framewise=True)
+        return rasterize(dmap).opts(width=width,
                                     height=height,
-                                    cmap=cmap, colorbar=colorbar, **plot_opts)
+                                    cmap=cmap, colorbar=colorbar, tools=tools, **plot_opts)
 
     def select(self, method='nearest', tolerance=None, region=None, path=None, **indexers):
         sel_obj = select(self._xrobj, method='nearest', tolerance=None, region=None, path=None, **indexers)
         return sel_obj.to_dataset()
 
-
-@xr.register_dataarray_accessor("pyfesom2")
-class FESOMDataArray(object):
-    def __init__(self, xr_dataarray):
-        self._xrobj = xr_dataarray
-        self._xrobj_stacked = None
-        self._mesh_as_mp = None
-        self._tree = None
-
-    @property
-    def stacked_data(self):
-        if not self._xrobj_stacked:
-            if "nod2" in self._xrobj.dims:
-                self._xrobj_stacked = self._xrobj.set_index(nod2=['lon', 'lat'])
-            else:
-                self._xrobj_stacked = self._xrobj.stack(nod2=['lon', 'lat'])
-        return self._xrobj_stacked
-
-    def select_region(self, region: Union[BoundingBox, Polygon]) -> xr.DataArray:
-        """
-        Selects data for a region. region can be specified as a Bounding Box [West, Soutn, North, East ] or
-        as shapely polygons or for multi region selection: a list of Polygons or MultiPolygon.
-        TODO: handle multi polygons as list or Multipolygon
-        Parameters
-        ----------
-        region
-
-        Returns
-        -------
-        xr.DataArray
-        """
-
-        if isinstance(region, Sequence) and len(region) == 4:
-            sel_polygon = box(*region)
-        elif isinstance(region, Polygon):
-            sel_polygon = region
-        else:
-            raise NotImplementedError(f'Supplied region data {region} is not yet supported')
-
-        if self._mesh_as_mp is None:
-            self._mesh_as_mp = MultiPoint(self.stacked_data.nod2.values.T)
-            # self._mesh_as_mp = asMultiPoint(self.stacked_data.nod2.values.T) # faster
-
-        # sel by index
-        # inds = [i for i, pt in enumerate(self._mesh_as_mp) if pt.within(sel_polygon)]
-        # #inds = list(set(inds)) # there are a few grids with dulplicates
-        # return self.stacked_data.isel(nod2=inds)
-
-        sel_indexer = np.asarray(self._mesh_as_mp.intersection(sel_polygon))
-        sel_indexer = list((lo, la) for lo, la in sel_indexer)
-        # sel_indexer = list(set((lo, la) for lo, la in sel_indexer))  # removing duplicates
-
-        if len(sel_indexer) == 0:
-            raise Exception('No region found')
-
-        return self._xrobj_stacked.sel(nod2=sel_indexer)
-
-    def select_point(self, lon, lat, method='nearest', tolerance=None):
-        from cartopy.crs import Geocentric, Geodetic
-        from scipy.spatial import cKDTree
-        if not method == 'nearest':
-            raise NotImplementedError("Spatial selection currently supports only nearest neighbor lookup")
-        geocentric_crs, geodetic_crs = Geocentric(), Geodetic()
-        if self._tree is None:
-            src_pts = geodetic_crs.transform_points(geodetic_crs, self._xrobj.lon.values, self._xrobj.lat.values)
-            self._tree = cKDTree(src_pts)
-        dst_pts = geodetic_crs.transform_points(geodetic_crs, np.array(lon), np.array(lat))
-
-        if tolerance is None:
-            # query is faster when no tolerance is specified
-            _, ind = self._tree.query(dst_pts)
-        else:
-            inds = self._tree.query_ball_point(dst_pts, r=tolerance, n_jobs=-1)[0]
-            # find min dist
-            ind = inds
-        return self._xrobj.isel(nod2=ind)
-
-    def sel(self, method='nearest', tolerance=None, region=None, path=None, **indexers):
-        """
-        TODO: support advanced xarray indexing
-        Parameters
-        ----------
-        method
-        tolerance
-        region
-        path
-        indexers
-
-        Returns
-        -------
-
-        """
-        lat = indexers.pop('lat', None)
-        lon = indexers.pop('lon', None)
-        lat_indexer = True if lat is not None else False
-        lon_indexer = True if lon is not None else False
-
-        if not method == "nearest":
-            raise NotImplementedError("Only nearest method is currently supported")
-        if (lat_indexer or lon_indexer) and (region is not None or path is not None):
-            # TODO: do this combinations better, doesn't check if path and region are both given
-            raise Exception("Onlu one option: lat, lon as indexer or path or region is supported")
-
-        if lat_indexer or lon_indexer:
-            if lat_indexer and lon_indexer:
-                ret_arr = self.select_point(lon, lat, method=method, tolerance=tolerance)
-            else:
-                raise NotImplementedError("Both lat, lon are needed as indexer, else use path or region")
-        elif region is not None:
-            ret_arr = self.select_region(region)
-        elif path is not None:
-            raise NotImplementedError('Path option is not implemented yet')
-        else:
-            print('in else')
-            ret_arr = self._xrobj
-
-        return ret_arr.sel(**indexers, method=method)
-
-    def isel(self, **indexers):
-        return self._xrobj.isel(**indexers)
-
-    def plot_map(self, *args, **kwargs):
-        import cartopy.crs as ccrs
-        import matplotlib.pyplot as plt
-        from matplotlib.tri import Triangulation
-
-        data = self._xrobj.squeeze()
-        if len(data.dims) > 1 or "nod2" not in data.dims:
-            raise Exception('Not a spatial dataset')
-
-        projection = kwargs.pop('projection', ccrs.PlateCarree())
-        ax = kwargs.pop('ax', plt.axes(projection=projection))
-
-        minv, maxv = data.min().values, data.max().values
-        tri = Triangulation(data.lon, data.lat, data.faces)
-        data = data.fillna(minv - 9999)  # make sure it is out of data bounds
-        levels = kwargs.pop('levels', np.linspace(minv, maxv, 100))
-        colorbar = kwargs.pop('colorbar', True)
-        pl = ax.tripcolor(tri, data.values.ravel(), levels=levels, transform=ccrs.PlateCarree(), *args, **kwargs)
-
-        if colorbar:
-            plt.colorbar(pl, ax=ax)
-        return pl
-
-    def plot_transect(self):
-        pass
-
-    def regrid(self):
-        pass
-
-    def regrid_like(self):
-        raise NotImplementedError
