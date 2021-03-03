340c76815d0abde826318750e64b7d6ac44448e0
suvarchal <suvarchal.kumar@gmail.com>
2021-03-03 06:54:42 +0100

    add doc string for fesom_like method

--- pyfesom2/datasets.py
+++ pyfesom2/datasets.py
@@ -1,9 +1,16 @@
-from typing import Sequence
-
 import xarray as xr
 
 import pyfesom2 as pf
 from pyfesom2.ut import get_no_cyclic
+import warnings
+from typing import Sequence, Optional
+import warnings
+from typing import Tuple, Optional
+
+import xarray as xr
+
+from . import load_mesh
+from .ut import get_no_cyclic
 
 datasets_dict = {"LCORE"  :
                      {"path"     : "https://swift.dkrz.de/v1/dkrz_02942825-0cab-44f3-ad37-80fd5d2e37e3/FESOM2_data/LCORE",
@@ -69,7 +76,7 @@ r42_dataset = R42()
 
 def fesom_mesh_to_xr(path: str, alpha: int = 0, beta: int = 0, gamma: int = 0) -> xr.Dataset:
     # nod2d = pd.read_csv(path+"/nod2d.out")
-    mesh = pf.load_mesh(path, abg=[alpha, beta, gamma])
+    mesh = load_mesh(path, abg=[alpha, beta, gamma])
     # midx = pd.MultiIndex.from_arrays([mesh.x2,mesh.y2], names=['lon','lat'])
     # with open(path+"/aux3d.out", "rt") as f:
     #    num_levels = int(f.readline())
@@ -110,13 +117,68 @@ def open_dataset(path_or_pattern: str, mesh_path: str, abg: Sequence = (50, 15, 
     return xr.merge([da, mesh])
 
 
-import warnings
-from typing import Sequence, Optional
+def fesom_like(spatial_size: int, spatial_extent: Tuple[float, float, float, float] = (-180, -90, 180, 90),
 
-
-def fesom_like(spatial_size: int, spatial_extent: Sequence[float, float, float, float] = (-180, -90, 180, 90),
                times: Optional[int] = None, levels: Optional[int] = None, holes: Optional[int] = None,
-               var_name: str = 'fesom_var'):
+               var_name: str = 'fesom_var') -> xr.Dataset:
+    """Returns a FESOM-like Xarray dataset for quick prototyping, development and benchmarking.
+
+    This method is most useful for developers, it provides a FESOM-like Xarray dataset for specified spatial_size. This
+    is very useful for creating FESOM-like datasets of various spatial sizes that might not be available in a pyfesom2
+    developer's system. The nodes(lon, lat) are uniformly distributed in ranges supplied by parameter spacial_extent.
+    Additional dimensions time and levels can be added with parameters times, levels. When dask is
+    available it also returns a dummy data variable uniformly distributed in range (0,1) if not  returns just a
+    coordinate dataset.
+
+    Parameters
+    ----------
+    spatial_size: int
+        spatial size of returned dataset.
+    spatial_extent: (min_lon, min_lat, max_lon, max_lat)
+        Sequence that defines the boundary of
+    times: int, array-like, optional
+        If provided as integer, times of this size are added as dimensions to dataset with monthly data starting in
+        2010 as values, a array of numpy datetime objects can be passed instead to control the time values.
+    levels: int, array-like, optional
+        If provided as integer, these many levels are added to the dataset. levels are linearly spaced from 0 to -6000.
+        A 1-d  array-like values of levels can be passed to control level values.
+    holes: int, optional
+        If provided, it randomly removes these number of faces to mimic mimic internal regions e.g,, land regions.
+    var_name: str
+        Name of data variable in returned dataset. This data variable is only returned if dask is present.
+
+    Returns
+    -------
+    xr.Dataset
+
+    Examples
+    --------
+    >>> fesom_like(100)
+    <xarray.Dataset>
+    Dimensions:    (nelem: 184, nod2: 100, three: 3)
+    Coordinates:
+        lon        (nod2) float64 4.858 -11.65 -69.51 40.91 ... 170.7 -101.2 108.0
+        lat        (nod2) float64 -28.72 -13.71 13.67 -76.21 ... -12.08 10.64 -22.7
+        faces      (nelem, three) int32 62 47 63 49 47 62 74 ... 46 36 56 56 57 46
+    Dimensions without coordinates: nelem, nod2, three
+    Data variables:
+        fesom_var  (nod2) float64 dask.array<chunksize=(100,), meta=np.ndarray>
+
+    To add time (or level) as dimension:
+
+    >>> fesom_like(100, times=10)
+    <xarray.Dataset>
+    Dimensions:    (nelem: 186, nod2: 100, three: 3, time: 10)
+    Coordinates:
+      * time       (time) datetime64[ns] 2010-01-31 2010-02-28 ... 2010-10-31
+        lon        (nod2) float64 -98.46 1.187 -27.48 93.83 ... 110.6 -108.8 -83.98
+        lat        (nod2) float64 59.19 47.07 -70.14 -41.33 ... -47.75 25.72 11.9
+        faces      (nelem, three) int32 67 21 14 12 43 45 91 ... 48 48 97 38 38 4 48
+    Dimensions without coordinates: nelem, nod2, three
+    Data variables:
+        fesom_var  (time, nod2) float64 dask.array<chunksize=(10, 100), meta=np.ndarray>
+
+    """
     from matplotlib.tri import Triangulation
     import pandas as pd
     import numpy as np
