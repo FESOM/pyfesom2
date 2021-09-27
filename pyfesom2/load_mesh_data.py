5c968069b431d33a770a13aea25ade1dafccb08c
Jan Streffing <jan.streffing@awi.de>
2021-09-27 15:50:53 +0200

    we always load as cftime objects, never as datetime64(ns). Avoids error when combining arrays

--- pyfesom2/load_mesh_data.py
+++ pyfesom2/load_mesh_data.py
@@ -517,7 +517,7 @@ def get_data(
         if not silent:
             print("Depth is None, 3d field will be returned")
 
-    dataset = xr.open_mfdataset(paths, combine="by_coords", **kwargs)
+    dataset = xr.open_mfdataset(paths, combine="by_coords", cftime=True, **kwargs)
     data = select_slices(dataset, variable, mesh, records, depth)
 
     if how == "mean":
