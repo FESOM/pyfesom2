b56a2d643347384af8392dcebe7aad6c641a4206
suvarchal <suvarchal.kumar@gmail.com>
2022-10-17 16:03:33 +0200

    push local dataset fixture into conftest and use session scope

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+from pyfesom2.datasets import open_dataset
+
+@pytest.fixture(scope="session", autouse=True)
+def local_dataset(request):
+    import os.path
+    cur_dir = os.path.dirname(__file__)#request.fspath)
+    data_path = os.path.join(cur_dir, "data", "pi-results", "*.nc")
+    mesh_path = os.path.join(cur_dir, "data", "pi-grid")
+    da = datasets.open_dataset(data_path, mesh_path=mesh_path)
+    yield da
+    da.close()
+
