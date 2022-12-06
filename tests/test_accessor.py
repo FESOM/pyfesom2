1c85ee54a9e77a5b6122d9250e3a5d8682c02c4d
suvarchal <suvarchal.kumar@gmail.com>
2022-12-07 00:16:19 +0100

    disable local/test dataset tests for accessors temporarily

--- tests/test_accessor.py
+++ tests/test_accessor.py
@@ -53,10 +53,14 @@ def random_nd_dataset(random_spatial_dataset):
 
 
 # merged dataset fixture using just 2 for now, in future add remote too
-@pytest.fixture(params=["local_dataset", "random_spatial_dataset"], scope='module', autouse=True)
-def dataset(local_dataset, random_spatial_dataset, request):
-    yield request.getfixturevalue(request.param)
+#@pytest.fixture(params=["local_dataset", "random_spatial_dataset"], scope='module', autouse=True)
+#def dataset(local_dataset, random_spatial_dataset, request):
+#    yield request.getfixturevalue(request.param)
 
+#TODO enable above to be compatible for CI/gh-actions
+@pytest.fixture(params=["random_spatial_dataset"], scope='module', autouse=True)
+def dataset(random_spatial_dataset, request):
+    yield request.getfixturevalue(request.param)
 
 @pytest.fixture(scope='module')
 def five_point_dataset():
