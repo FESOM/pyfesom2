13b3e25593faf23e43f38eed2a862fceeab465a1
Paul Gierz <pgierz@awi.de>
2025-04-02 15:35:57 +0200

    Bump version: 0.2.0 → 0.3.0

--- pyfesom2/__init__.py
+++ pyfesom2/__init__.py
@@ -8,17 +8,17 @@
 
 __author__ = """FESOM team"""
 __email__ = "koldunovn@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
+from .accessor import FESOMDataArray as _FESOMDataArray
+from .ascii_to_netcdf import read_fesom_ascii_grid, write_mesh_to_netcdf
+from .climatology import *
+from .datasets import open_dataset
+from .diagnostics import *
+from .fesom2GeoFormat import *
 from .load_mesh_data import *
 from .plotting import *
-from .climatology import *
 from .regridding import *
 from .transect import *
-from .diagnostics import *
-from .ut import *
-from .fesom2GeoFormat import *
-from .datasets import open_dataset
-from .accessor import FESOMDataArray as _FESOMDataArray
 from .transport import cross_section_transport
-from .ascii_to_netcdf import read_fesom_ascii_grid, write_mesh_to_netcdf
+from .ut import *
